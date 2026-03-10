e584aa6c48472f299b41a03e325dba01dfb7ee95
ZiaudeenSulthan-ship-it <Ziaudeen.Sulthan@cognizant.com>
2026-03-11 00:12:56 +0530

    Update streamlit_app.py
    PACK

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
-#new section to add smoothie nutrition information
 import requests  
-smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
