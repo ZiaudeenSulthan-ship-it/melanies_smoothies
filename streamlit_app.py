6b4676bbb453ba8e8d36838fa8ccb246fc0854ad
ZiaudeenSulthan-ship-it <Ziaudeen.Sulthan@cognizant.com>
2026-03-11 00:10:30 +0530

    Update streamlit_app.py
    upDATE URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ if time_to_insert:
 
 #new section to add smoothie nutrition information
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response.json())
